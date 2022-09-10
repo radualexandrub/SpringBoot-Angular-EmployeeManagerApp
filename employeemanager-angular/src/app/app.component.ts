506ab93bb64d7fe5280d2308975262fac784ebdc
Radu-Alexandru Bulai <radualexandrubulai@gmail.com>
2022-09-10 17:15:50 +0300

    Create Angular UI, create Add/Edit/Delete HTML Modals, create onAddEmployee functionality, create CORS filter on Back-End

--- employeemanager-angular/src/app/app.component.ts
+++ employeemanager-angular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
 
@@ -25,6 +26,44 @@ export class AppComponent implements OnInit {
       },
       (error: HttpErrorResponse) => {
         console.log(error.message);
+      }
+    );
+  }
+
+  public onOpenModal(employee: Employee | null, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+
+    if (mode == 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if (mode == 'edit') {
+      button.setAttribute('data-target', '#editEmployeeModal');
+    }
+    if (mode == 'delete') {
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+
+    container?.appendChild(button);
+    button.click();
+    button.remove();
+  }
+
+  public onAddEmployee(addForm: NgForm): void {
+    let methodName = 'onAddEmployee() ';
+    this.employeeService.addEmployee(addForm.value).subscribe(
+      (response: Employee) => {
+        console.debug(
+          methodName + 'Response Received: ' + JSON.stringify(response)
+        );
+        this.getEmployees();
+        document.getElementById('add-employee-form-close')?.click();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
       }
     );
   }
--- employeemanager-angular/src/app/app.module.ts
+++ employeemanager-angular/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
