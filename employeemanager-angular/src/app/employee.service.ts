4b05deabae6ca1b59be4cd85ddbfd5cb15ac1de0
Radu-Alexandru Bulai <radualexandrubulai@gmail.com>
2022-09-28 20:08:03 +0300

    Refactor: Change API URLs with RESTful URLs format

--- employeemanager-angular/src/app/employee.service.ts
+++ employeemanager-angular/src/app/employee.service.ts
@@ -8,47 +8,37 @@ import { Employee } from './employee';
   providedIn: 'root',
 })
 export class EmployeeService {
-  private apiServerUrl = environment.apiBaseUrl;
+  private apiServerUrl = environment.apiBaseUrl + '/api/employees';
 
   constructor(private http: HttpClient) {}
 
   public getEmployees(): Observable<any> {
     const methodName = 'getEmployees() ';
     console.debug(methodName + 'Request Sent');
-    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+    return this.http.get<Employee[]>(`${this.apiServerUrl}`);
   }
 
   public getEmployeeById(employeeId: number): Observable<Employee> {
     const methodName = 'getEmployeeById() ';
     console.debug(methodName + 'Request Sent: ' + employeeId);
-    return this.http.get<Employee>(
-      `${this.apiServerUrl}/employee/find/${employeeId}`
-    );
+    return this.http.get<Employee>(`${this.apiServerUrl}/${employeeId}`);
   }
 
   public addEmployee(employee: Employee): Observable<Employee> {
     const methodName = 'addEmployee() ';
     console.debug(methodName + 'Request Sent: ' + JSON.stringify(employee));
-    return this.http.post<Employee>(
-      `${this.apiServerUrl}/employee/add`,
-      employee
-    );
+    return this.http.post<Employee>(`${this.apiServerUrl}/new`, employee);
   }
 
   public updateEmployee(employee: Employee): Observable<Employee> {
     const methodName = 'updateEmployee() ';
     console.debug(methodName + 'Request Sent: ' + JSON.stringify(employee));
-    return this.http.put<Employee>(
-      `${this.apiServerUrl}/employee/update`,
-      employee
-    );
+    return this.http.put<Employee>(`${this.apiServerUrl}/update`, employee);
   }
 
   public deleteEmployee(employeeId: number): Observable<void> {
     const methodName = 'deleteEmployee() ';
     console.debug(methodName + 'Request Sent: ' + employeeId);
-    return this.http.delete<void>(
-      `${this.apiServerUrl}/employee/delete/${employeeId}`
-    );
+    return this.http.delete<void>(`${this.apiServerUrl}/${employeeId}`);
   }
 }
--- src/main/java/com/radubulai/employeemanager/EmployeeResource.java
+++ src/main/java/com/radubulai/employeemanager/EmployeeResource.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/api/employees")
 public class EmployeeResource {
     private final EmployeeService employeeService;
 
@@ -18,19 +18,19 @@ public class EmployeeResource {
         this.employeeService = employeeService;
     }
 
-    @GetMapping("/all")
+    @GetMapping("")
     public ResponseEntity<List<Employee>> getAllEmployees() {
         List<Employee> employees = employeeService.findAllEmployees();
         return new ResponseEntity<>(employees, HttpStatus.OK);
     }
 
-    @GetMapping("/find/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id) {
         Employee employee = employeeService.findEmployeeById(id);
         return new ResponseEntity<>(employee, HttpStatus.OK);
     }
 
-    @PostMapping("/add")
+    @PostMapping("/new")
     public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
         Employee newEmployee = employeeService.addEmployee(employee);
         return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
@@ -43,7 +43,7 @@ public class EmployeeResource {
     }
 
     @Transactional
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id) {
         employeeService.deleteEmployee(id);
         return new ResponseEntity<>(HttpStatus.OK);
