10ded85eaf74ea759115e4a930b27cb3b8486256
Radu-Alexandru Bulai <radualexandrubulai@gmail.com>
2022-09-28 22:10:24 +0300

    Refactor: Move Angular service files to app/services folder

--- employeemanager-angular/src/app/app.component.ts
+++ employeemanager-angular/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Employee } from './employee';
-import { EmployeeService } from './employee.service';
+import { EmployeeService } from './services/employee.service';
 
 @Component({
   selector: 'app-root',
--- employeemanager-angular/src/app/services/employee.service.ts
+++ employeemanager-angular/src/app/services/employee.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Employee } from './employee';
+import { Employee } from '../employee';
 
 @Injectable({
   providedIn: 'root',
