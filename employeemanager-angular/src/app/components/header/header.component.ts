73786415aa9aa06984e5f16aadca734247bff74d
radu <radualexandrubulai@gmail.com>
2022-10-06 23:40:05 +0300

    Add Dark-Mode Functionality in Angular App

--- employeemanager-angular/src/app/components/header/header.component.ts
+++ employeemanager-angular/src/app/components/header/header.component.ts
@@ -10,7 +10,25 @@ import { Employee } from 'src/app/employee';
 export class HeaderComponent implements OnInit {
   constructor(private appComponent: AppComponent) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (localStorage.getItem("theme") === null) {
+      localStorage.setItem("theme", "light");
+      document.documentElement.setAttribute("data-theme", "light")
+    } else if (localStorage.getItem("theme") === "dark") {
+      document.documentElement.setAttribute("data-theme", "dark")
+    }
+  }
+
+  onToggleDarkTheme(): void {
+    const currentTheme = localStorage.getItem("theme");
+    if (currentTheme === "light") {
+      localStorage.setItem("theme", "dark");
+      document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+      localStorage.setItem("theme", "light");
+      document.documentElement.setAttribute("data-theme", "light");
+    }
+  }
 
   onOpenModal(employee: Employee, mode: string): void {
     this.appComponent.onOpenModal(employee, mode);
